fdcfe609ae26fbdfa3ab5aa7021e22ec4dc42491
Ahmed Mohammed <fox_it@yahoo.com>
2022-08-26 13:28:27 +0200

    add BackUp every minute

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,11 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('backup:run')->everyMinute();
+        // $schedule->command('inspire')->hourly();->everyTwoMinutes();
+        // $schedule->command('backup:clean')->everyTwoMinutes();
+        // $schedule->command('backup:clean')->daily()->at('01:00');
+        // $schedule->command('backup:run')->daily()->at('02:00');
     }
 
     /**
